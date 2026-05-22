0e06b69465ca540da4c5660c03e1bf5f63f0960a
Saiyam-Sandhir-Jain <saiyam.sandhir.jain@gmail.com>
2026-05-22 11:03:41 +0530

    Striver A2Z: Learn Bit Manipulation -- Check if a number is odd or not

--- src/main/java/bit_manipulation/CheckUtils.java
+++ src/main/java/bit_manipulation/CheckUtils.java
@@ -4,4 +4,8 @@ public class CheckUtils {
     public static boolean isBitSet(int num, int i) {
         return (num & (1 << i)) != 0;
     }
+
+    public static boolean isOdd(int num) {
+        return (num & 1) != 0;
+    }
 }
---
+++ src/main/java/bit_manipulation/Main.java
@@ -0,0 +1,30 @@
+package main.java.bit_manipulation;
+
+import java.util.Scanner;
+import main.java.bit_manipulation.CheckUtils;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        {
+            System.out.println("##### Check if i-th bit is set or not #####");
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+            System.out.print("Enter the value of i: ");
+            if (CheckUtils.isBitSet(num, sc.nextInt())) {
+                System.out.println("The i-th bit is set in the given number.");
+            } else System.out.println("The i-th bit is not set in the given number.");
+            System.out.println();
+        }
+
+        {
+            System.out.println("##### Check if a number is odd or not #####");
+            System.out.print("Enter a number: ");
+            if (CheckUtils.isOdd(sc.nextInt())) {
+                System.out.println("The given number is odd.");
+            } else System.out.println("The given number is not odd.");
+            System.out.println();
+        }
+    }
+}
